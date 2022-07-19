bc1a19947fab942d42d8d20e06770b53a7f485d4
KelvinSilvaDev <63024154+KelvinSilvaDev@users.noreply.github.com>
2022-07-19 17:29:59 -0300

    feat: estilizacao do menu

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,15 +1,30 @@
 import styled from "styled-components";
 
 export const MenuWrapp = styled.div`
-  padding: 1.2rem;
   width: 100%;
+  height: 90px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  position: sticky;
+  background: #000000c2;
+  top: 0;
+  z-index: 9999;
   nav {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
     a {
-      padding: 1rem;
+      padding: 1.2rem;
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+
       :hover {
         color: #faa0f0;
         background: #fff;
